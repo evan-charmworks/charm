40b797fbf1fe8853df03ea78d3e7d18be7e51b55
Ronak Buch <rabuch2@illinois.edu>
2020-07-22 04:31:45 -0500

    Add API and implementation for phase vector load storage

--- src/ck-core/cklocation.C
+++ src/ck-core/cklocation.C
@@ -1551,6 +1551,10 @@ double CkMigratable::getObjTime() {
 	return myRec->getObjTime();
 }
 
+const std::vector<LBRealType> CkMigratable::getObjVectorLoad() const {
+  return myRec->getObjVectorLoad();
+}
+
 #if CMK_LB_USER_DATA
 /**
 * Use this method to set user specified data to the lbdatabase.
@@ -1572,6 +1576,10 @@ void *CkMigratable::getObjUserData(int idx) {
 	return myRec->getObjUserData(idx);
 }
 #endif
+
+void CkMigratable::CkLBSetPhase(int phase) {
+  myRec->CkLBSetPhase(phase);
+}
 
 void CkMigratable::clearMetaLBData() {
 //  if (can_reset) {
@@ -1779,10 +1787,16 @@ void CkMigratable::CkAddThreadListeners(CthThread tid, void *msg)
 #else
 void CkMigratable::setObjTime(double cputime) {}
 double CkMigratable::getObjTime() {return 0.0;}
+const std::vector<LBRealType> CkMigratable::getObjVectorLoad() const
+{
+  return std::vector<LBRealType>();
+};
 
 #if CMK_LB_USER_DATA
 void *CkMigratable::getObjUserData(int idx) { return NULL; }
 #endif
+
+void CkMigratable::CkLBSetPhase(int phase) {}
 
 /* no load balancer: need dummy implementations to prevent link error */
 void CkMigratable::CkAddThreadListeners(CthThread tid, void *msg)
@@ -1871,11 +1885,18 @@ double CkLocRec::getObjTime() {
         lbmgr->GetObjLoad(ldHandle, walltime, cputime);
         return walltime;
 }
+const std::vector<LBRealType> CkLocRec::getObjVectorLoad() const {
+  return lbmgr->GetObjVectorLoad(ldHandle);
+}
 #if CMK_LB_USER_DATA
 void* CkLocRec::getObjUserData(int idx) {
         return lbmgr->GetDBObjUserData(ldHandle, idx);
 }
 #endif
+
+void CkLocRec::CkLBSetPhase(int phase) {
+  lbmgr->SetPhase(ldHandle, phase);
+}
 #endif
 
 // Attempt to destroy this record. If the location manager is done with the
